7b00c5c3f4b437fa5c890bf37a2ea9ebbbae8f96
胡勇 <apple@appledemacbook-pro-2.local>
2018-05-03 10:26:17 +0800

    Initial Commit

---
+++ HelloOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloOpenGL
+//
+//  Created by apple on 2018/5/3.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
